4967f1967fbdffbc3e7867dea0d41455b480725f
notABluesSinger <rob@whatagoodbot.com>
2022-10-29 21:41:49 +0100

    WIP Commit

--- examples/client.js
+++
@@ -1,8 +0,0 @@
-import { clientCreds, users } from '../src/index.js'
-
-const userService = new users.UserService('localhost:50051', clientCreds)
-
-userService.getUsers({}, (error, response) => {
-  if (error) console.log(error)
-  console.log(response)
-})
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import grpc from '@grpc/grpc-js'
 import protoLoader from '@grpc/proto-loader'
 
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const protoOptions = {
   keepCase: true,
   longs: String,
@@ -12,6 +17,9 @@ export const server = new grpc.Server()
 export const serverCreds = grpc.ServerCredentials.createInsecure()
 export const clientCreds = grpc.credentials.createInsecure()
 
-export const config = grpc.loadPackageDefinition(protoLoader.loadSync('./src/protos/config.proto', protoOptions))
-export const strings = grpc.loadPackageDefinition(protoLoader.loadSync('./src/protos/strings.proto', protoOptions))
-export const users = grpc.loadPackageDefinition(protoLoader.loadSync('./src/protos/users.proto', protoOptions))
+export const album = grpc.loadPackageDefinition(protoLoader.loadSync(`${__dirname}/protos/album.proto`, protoOptions))
+export const artist = grpc.loadPackageDefinition(protoLoader.loadSync(`${__dirname}/protos/artist.proto`, protoOptions))
+export const config = grpc.loadPackageDefinition(protoLoader.loadSync(`${__dirname}/protos/config.proto`, protoOptions))
+export const strings = grpc.loadPackageDefinition(protoLoader.loadSync(`${__dirname}/protos/strings.proto`, protoOptions))
+export const users = grpc.loadPackageDefinition(protoLoader.loadSync(`${__dirname}/protos/users.proto`, protoOptions))
+export const wiki = grpc.loadPackageDefinition(protoLoader.loadSync(`${__dirname}/protos/wiki.proto`, protoOptions))
